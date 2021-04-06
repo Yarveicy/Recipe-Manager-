9922e2992337c5f2aca9fd5fe18bdb605bc7867e
yarveicy <yarveicy@ualberta.ca>
2021-04-06 14:37:41 -0700

    Modify UML Diagram Based on Discussion

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -17,7 +17,7 @@ public class JsonWriter {
     private PrintWriter writer;
     private String destination;
 
-    // EFFECTS: constructs writer to write to destination file
+    // EFFECTS: constructs writer to write to destination file//
     public JsonWriter(String destination) {
         this.destination = destination;
     }
